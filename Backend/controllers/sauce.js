d88e293f92ee396d880d145e5330afd646f0143b
Clara Bernadou <clarabernadou56690@gmail.com>
2022-03-16 13:56:46 +0100

    Modifications

--- Backend/controllers/sauce.js
+++ Backend/controllers/sauce.js
@@ -96,3 +96,47 @@ exports.getAllSauces = (req, res, next) => {
     }
   );
 };
+
+//LIKE OR DISLIKE
+exports.likeOrNot = (req, res, next) => {
+  if (req.body.like === 1) {
+      Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } })
+          .then((sauce) => 
+            res.status(200).json({ message: 'Add like' }))
+          .catch(error => 
+              res.status(400).json({ error }))
+  } else if (req.body.like === -1) {
+    Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } })
+          .then((sauce) => 
+            res.status(200).json({ message: 'Add dislike' }))
+          .catch(error => 
+              res.status(400).json({ error }))
+  } else {
+Sauce.findOne({ _id: req.params.id })
+  .then(sauce => {
+    if (sauce.usersLiked.includes(req.body.userId)) {
+    Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+      .then(
+        (sauce) => { 
+          res.status(200).json({ message: 'Like deleted' }) })
+//IF ELSE
+.catch(
+  error => res.status(400).json({ error }))
+} else if (sauce.usersDisliked.includes(req.body.userId)) {
+  Sauce.updateOne(
+    { _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+      .then(
+        (sauce) => { 
+          res.status(200).json({ message: 'Dislike deleted' }) })
+//IF ELSE
+.catch(
+  error => 
+    res.status(400).json({ error }))
+        }
+  })
+//IF ELSE
+.catch(
+  error => 
+    res.status(400).json({ error }))
+  }
+}
--- Backend/routes/sauces.js
+++ Backend/routes/sauces.js
@@ -11,5 +11,6 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, multer, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeOrNot);
 
 module.exports = router;
